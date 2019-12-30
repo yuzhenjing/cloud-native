2d544cc490388e3193891a2170b57024de60c703
137819913@qq.com <yzjqq995>
2019-12-30 20:29:50 +0800

    添加演示

--- native-provider/src/main/java/com/cloud/ProviderApplication.java
+++ native-provider/src/main/java/com/cloud/ProviderApplication.java
@@ -1,6 +1,5 @@
 package com.cloud;
 
-import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
---
+++ native-provider/src/main/java/com/cloud/controller/NacosController.java
@@ -0,0 +1,32 @@
+package com.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class NacosController {
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${es.url}")
+    private String value;
+
+
+    @RequestMapping("/config/{name}")
+    public String config(@PathVariable String name) {
+        return environment.getProperty(name);
+    }
+
+    @RequestMapping("/config")
+    public String value() {
+        return this.value;
+    }
+
+}
